b17e64c2af79545ef7aa6f693640ebbf4f900bfc
Heema Goratela <heema@simformsolutions.com>
2023-02-13 09:40:08 +0530

    Practice files committed

---
+++ challenge_prac/array.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var arr [4]int
+	arr[0] = 1
+	arr[1] = 2
+	fmt.Println(arr)
+	var arr1 [4]int = [4]int{24, 4, 5, 6}
+	fmt.Print(arr1)
+	var strArr = [4]string{}
+	fmt.Print(strArr)
+
+}
